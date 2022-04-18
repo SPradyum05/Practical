0e605adf1643fe80f3d7354a1e30504964f46ef6
SPradyum05 <103924186+SPradyum05@users.noreply.github.com>
2022-04-18 13:33:28 +0530

    Create 2.cpp

---
+++ 2.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b;
+    int c, d;
+    int e, f;
+    int g;
+    
+    cout<<"Enter First Value: ";
+    cin>>a;
+    
+    cout<<"Enter Second Value: ";
+    cin>>b;
+    
+    c=a+b;
+    d=a-b;
+    e=a*b;
+    f=a/b;
+    g=a%b;
+    
+    cout<<"addition="<<c<<endl;
+    cout<<"subtraction="<<d<<endl;
+    cout<<"multiplication="<<e<<endl;
+    cout<<"division="<<f<<endl;
+    cout<<"modulas="<<g<<endl;
+    
+    
+
+    return 0;
+}
